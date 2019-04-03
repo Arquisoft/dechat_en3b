2754dee7ad3acf21bda17857c35d9e1e351bb92d
Juan Sánchez-Ocaña Sánchez <juan_sanchez3@hotmail.com>
2019-04-03 21:06:25 +0200

    Added modal chat creator template

---
+++ src/app/create-chat-modal/create-chat-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-chat-modal',
+  templateUrl: './create-chat-modal.component.html',
+  styleUrls: ['./create-chat-modal.component.css']
+})
+export class CreateChatModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
