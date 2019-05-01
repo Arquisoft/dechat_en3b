a3dc15b649ce26ec8c930e7b4e0e5432c7c27202
Daniel <UO251399@uniovi.es>
2019-05-01 23:40:01 +0200

    update doc's pics for the deployment

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('copyImages', (done) => {
 })
 
 gulp.task('adoc2html', function (cb) {
-  const command = `${path.resolve('node_modules/.bin/asciidoctorjs')} adocs/index.adoc -a toc=left --destination-dir docs`
+  const command = `asciidoctor src/docs/index.adoc -a toc=left --destination-dir docs`
   exec(command, function (err, stdout, stderr) {
     console.log(stdout)
     console.log(stderr)
